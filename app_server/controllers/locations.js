d4e7ec6d200d4ba161d480628b3ec6a6ab09e3f0
rkmahli <rajkishoremahli@gmail.com>
2019-06-17 20:16:38 +0530

    Location, Others controllers added
    In Progress

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,11 @@
+const homeList = (req, res) => {};
+
+const locationInfo = (req, res) => {};
+
+const addReview = (req, res) => {};
+
+module.exports = {
+    homeList,
+    locationInfo,
+    addReview
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+// const ctrlMain = require('../controllers/main');
+const ctrlLocations = require('../controllers/locations');
+const ctrlOthers = require('../controllers/others');
+
+/* Location pages */
+router.get('/', ctrlLocations.homeList);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
